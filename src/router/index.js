dda4a26e290bf3217f733b34ba4d4d9f59703c63
Renan Santana <renan_ota@hotmail.com>
2021-06-05 23:38:13 -0300

    Adiciona título da página ao browser

--- src/router/index.js
+++ src/router/index.js
@@ -11,21 +11,37 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'Público Geral Enquetes'
+    }
   },
   {
     path: '/master',
     name: 'Admin',
     component: Admin,
+    meta: {
+      title: 'Criação de Enquete'
+    }
   },
   {
     path: '/master/results',
     name: 'Resultados',
     component: Results,
+    meta: {
+      title: 'Resultados'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.afterEach((to) => {
+  // Seta o título da página no browser
+  if (to.meta && to.meta.title) {
+    document.title = to.meta.title
+  }
+});
 
 export default router
