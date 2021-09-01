1a79cdf6fc5c568d771a2c9cb3095dcd5320df10
Renan Santana <renan_ota@hotmail.com>
2021-09-01 18:45:36 -0300

    Implementa modo history no router

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require('express')
 const serveStatic = require("serve-static")
-const path = require('path');
+const path = require('path')
+const history = require('connect-history-api-fallback')
 
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+app = express()
+app.use(history())
+app.use(serveStatic(path.join(__dirname, 'dist')))
+const port = process.env.PORT || 80
+app.listen(port)
